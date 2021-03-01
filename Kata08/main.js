d3811ab2d3ffb75a18e2bf50463a267c13f13213
jessemaa <mewwbae@gmail.com>
2021-03-01 09:38:26 +0200

    Kata08

---
+++ Kata08/main.js
@@ -0,0 +1,9 @@
+function getLength(array) {
+	return array.flat(Infinity).length
+}
+
+console.log(getLength([])) // 0
+console.log(getLength([1, [2, 3]])) // 3
+console.log(getLength([1, [2, [3, 4]]])) // 4
+console.log(getLength([1, [2, [3, [4, [5, 6]]]]])) // 6
+console.log(getLength([1, [2], 1, [2], 1])) // 5
